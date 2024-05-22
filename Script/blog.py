c338960ec565c266fdc063a103bdd9ffb0dfafa0
mikiiiss <mihreykiros23@gmail.com>
2024-05-22 19:00:53 +0300

    folder rearrangement

---
+++ Script/blog.py
@@ -0,0 +1,36 @@
+from telethon import TelegramClient
+import csv
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+api_id = os.getenv('TG_API_ID')
+api_hash = os.getenv('TG_API_HASH')
+phone = os.getenv('phone')
+channel_username = 'Eliasmeserett' 
+
+
+
+client = TelegramClient('blog', api_id, api_hash)
+
+
+async def main():
+
+    await client.start()
+    #if not await client.is_user_authorized():
+     #   await client.send_code_request(phone)
+      #  await client.sign_in(phone, input('Enter the code: '))
+
+    entity = await client.get_entity(channel_username)
+
+    with open ('blog_data.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(['id', 'message', 'date'])
+
+        async for message in client.iter_messages(entity, limit =2000):
+            writer.writerow([message.id, message.message, message.date])
+
+
+with client:
+    client.loop.run_until_complete(main())
